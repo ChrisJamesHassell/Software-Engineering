174557916a539834562029944c1155c233e12676
Micah Mitchell <mikah1337@gmail.com>
2018-10-21 02:40:13 -0500

    Fixing routes, preparing for Redux

--- PlatypusWeb/src/index.js
+++ PlatypusWeb/src/index.js
@@ -1,79 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-import { routes, RouteWithSubRoutes } from './scripts/routes';
-import Login from './scripts/components/Pages/Login';
-import { Dashboard } from './scripts/components/Pages/Dashboard';
-import logo from './images/icons/logo_fill_white.svg';
-
+// import { Provider } from 'react-redux';
+// import configureStore from './scripts/store/configureStore';
+import { routes } from './scripts/routes';
+import Main from './scripts/components/Pages/Main';
 import './css/style.css';
 
-const Main = () => (
-    <Router>
-        <div id="container">
-            <Navbar collapseOnSelect style={{ marginBottom: '0', borderRadius: '0' }}>
-                <Navbar.Header>
-                    <Navbar.Brand id='logo-brand'>
-                        <img src={logo}></img>
-                        <Link to="/"><span id='brand-platy'>platy</span><span id='brand-pus'>pus</span></Link>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <Nav>
-                        <LinkContainer to="/dashboard"><NavItem eventKey={1}>Dashboard</NavItem></LinkContainer>
-                        <LinkContainer to="/"><NavItem eventKey={2}>Home (login)</NavItem></LinkContainer>
-                        <NavDropdown eventKey={3} title='Dropdown' id='basic-nav-dropdown'>
-                            <MenuItem eventKey={3.1}>Something1</MenuItem>
-                            <MenuItem eventKey={3.2}>Something2</MenuItem>
-                            <MenuItem divider />
-                            <MenuItem eventKey={3.3}>Something3</MenuItem>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-            <Route exact path="/" component={Login} />
-            {routes.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)}
-        </div>
-    </Router>
-);
+
+//const store = configureStore();
+const baseURI = "https://www.platypus.null-terminator.com/api/";
 
 render(
-    <Main />
-    , document.getElementById('root')
-);
-
-// <Route exact path="/" component={Login} />
-// <Route path="/dashboard" component={Dashboard} />
-// <Route path="/login" component={Login} />
-/*const App = () => {
-    return (
-        <div id="container">
-            <AppNavbar />
-            <AppJumbo />
-        </div>
-    );
-}*/
-/*
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <div id="container">
-                <AppNavbar />
-                <AppJumbo />
-            </div>
-        )
-    }
-}
-
-ReactDom.render(
-    <App />,
+    <Main routes={routes} baseURI={baseURI} />,
     document.getElementById('root')
 );
-*/
+
+// render(
+//     <Provider store={store}>
+//         <Main routes={routes} />
+//     </Provider>, 
+//     document.getElementById('root')
+// );
 
