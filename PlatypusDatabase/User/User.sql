0c8f90d43a72fb019c075503c6526b2c6ae7747d
nathan-robson <31709195+nathan-robson@users.noreply.github.com>
2018-10-20 22:58:28 -0500

    feat(User): add email field, split name to first/last

--- PlatypusDatabase/User/User.sql
+++ PlatypusDatabase/User/User.sql
@@ -6,10 +6,12 @@
  */
 
 CREATE TABLE User(
-  user_id       INT         NOT NULL UNSIGNED AUTO_INCREMENT,
-  name          VARCHAR(32) NOT NULL, -- User's name, provided on acct create
-  username      VARCHAR(32) NOT NULL, -- User's username, provided on acct create
-  password      VARCHAR(32) NOT NULL, -- User's most recent password
+  user_id       INT          NOT NULL UNSIGNED AUTO_INCREMENT,
+  firstName     VARCHAR(32)  NOT NULL, -- User's first name, provided on acct create
+  lastName      VARCHAR(32) NOT NULL,  -- User's last name, provided on acct create
+  email         VARCHAR(128) NOT NULL, -- User's email, provided on acct create
+  username      VARCHAR(32)  NOT NULL, -- User's username, provided on acct create
+  password      VARCHAR(32)  NOT NULL, -- User's most recent password
 
   PRIMARY KEY ( user_id )
 );
