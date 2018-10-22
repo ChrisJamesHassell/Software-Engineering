01a03a27db7b5fe9841da37c83f8535ad4a8338c
Micah Mitchell <mikah1337@gmail.com>
2018-10-21 19:25:25 -0500

    fixes with router for login page

--- PlatypusWeb/src/scripts/components/Pages/Login.js
+++ PlatypusWeb/src/scripts/components/Pages/Login.js
@@ -5,46 +5,50 @@ import { Link, Route, Switch } from 'react-router-dom';
 import { RouteWithSubRoutes } from '../../routes';
 import LoginForm from '../Forms/LoginForm';
 import SignupForm from '../Forms/SignupForm';
+import logo from '../../../images/icons/logo_fill_white.svg';
 
-const Login = (props) => (
-    <Grid id='row-container'>
-        <Row id='row-space'>
+const Login = (props) => {
+    var logoUrl = window.location.origin + '/' + logo;
+    return (
+        <Grid id='row-container'>
+            <Row id='row-space'>
 
-        </Row>
-        <Row id='login'>
-            <Col id='login-extra' xsHidden md={8}>
-                <div>
-                    <h1>Organize.</h1>
-                    <h1>Plan.</h1>
-                    <h1>Live.</h1>
-                    <p>
-                        Hey, adulting is hard. We get it. That's why Platypus provides
-                        a sleek, modern interface to help you adult at maximum efficiency.
-                    </p>
-                    <p>
-                        <Button bsStyle='success' bsSize='large'>Learn More</Button>
-                    </p>
-                </div>
-            </Col>
-            <Col id='login-logo' smHidden mdHidden lgHidden xs={12}>
-                <img src={props.logo} />
-            </Col>
-            <Col id='login-creds' xs={12} md={4}>
-                <div>
-                    <p>
-                        <LinkContainer to="/"><Button bsStyle="link" disabled={props.match.isExact}>Login</Button></LinkContainer> or
-                        <LinkContainer to="/signup"><Button bsStyle="link" disabled={!props.match.isExact}> Sign Up</Button></LinkContainer>
-                    </p>
-                    <Switch>
-                        <Route exact path="/" render={(props) => <LoginForm {...props} />} />
-                        <Route path="/signup" render={(props) => <SignupForm {...props} />} />
-                    </Switch>
+            </Row>
+            <Row id='login'>
+                <Col id='login-extra' xsHidden md={8}>
+                    <div>
+                        <h1>Organize.</h1>
+                        <h1>Plan.</h1>
+                        <h1>Live.</h1>
+                        <p>
+                            Hey, adulting is hard. We get it. That's why Platypus provides
+                            a sleek, modern interface to help you adult at maximum efficiency.
+                        </p>
+                        <p>
+                            <Button bsStyle='success' bsSize='large'>Learn More</Button>
+                        </p>
+                    </div>
+                </Col>
+                <Col id='login-logo' smHidden mdHidden lgHidden xs={12}>
+                    <img src={logoUrl} />
+                </Col>
+                <Col id='login-creds' xs={12} md={4}>
+                    <div>
+                        <p>
+                            <LinkContainer to="/"><Button bsStyle="link" disabled={props.match.isExact}>Login</Button></LinkContainer> or
+                            <LinkContainer to="/login/signup"><Button bsStyle="link" disabled={!props.match.isExact}> Sign Up</Button></LinkContainer>
+                        </p>
+                        <Switch>
+                            <Route exact path="/" render={(props) => <LoginForm {...props} />} />
+                            <Route path="/login/signup" render={(props) => <SignupForm {...props} />} />
+                        </Switch>
 
-                </div>
-            </Col>
-        </Row>
-    </Grid>
-)
+                    </div>
+                </Col>
+            </Row>
+        </Grid>
+    )
+}
 
 export default Login;
 
