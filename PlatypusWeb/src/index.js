122c1aca95c8e80a7c299ab8955589e7ef84ad15
Micah Mitchell <mikah1337@gmail.com>
2018-10-22 09:34:39 -0500

    Moving over to boilerplate because Jonathan hates me

--- PlatypusWeb/src/index.js
+++ PlatypusWeb/src/index.js
@@ -1,9 +1,9 @@
+import * as serviceWorker from './serviceWorker';
 import React from 'react';
 import { render } from 'react-dom';
-// import { Provider } from 'react-redux';
-// import configureStore from './scripts/store/configureStore';
 import { routes } from './scripts/routes';
-import Main from './scripts/components/Pages/Main';
+import App from './scripts/components/Pages/App';
+import './css/global/bootstrap.css';
 import './css/style.css';
 import logo from './images/icons/logo_fill_white.svg'
 
@@ -12,14 +12,11 @@ import logo from './images/icons/logo_fill_white.svg'
 const baseURI = "https://www.platypus.null-terminator.com/api/";
 
 render(
-    <Main routes={routes} logo={logo} baseURI={baseURI} />,
+    <App routes={routes} logo={logo} baseURI={baseURI} />,
     document.getElementById('root')
 );
 
-// render(
-//     <Provider store={store}>
-//         <Main routes={routes} />
-//     </Provider>, 
-//     document.getElementById('root')
-// );
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
--- PlatypusWeb/src/scripts/components/Pages/App.js
+++ PlatypusWeb/src/scripts/components/Pages/App.js
@@ -1,27 +1,42 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Login from "./Login";
-import {Dashboard} from './Dashboard';
+import { BrowserRouter as Router, Route, Link, Redirect, withRouter } from "react-router-dom";
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { RouteWithSubRoutes, routes } from '../../routes';
+import { Dashboard } from './Dashboard';
+import Login from './Login';
+import AppLogoHeader from '../Navbar/AppLogoHeader';
 
-/*const App = (props) => {
-    return (
+/**************************************/
+//  We want to:
+// ----------------------------------- //
+//  1. Check to see if user is logged in (dash)
+//  2. If not, display login page
+//  3. If yes, display Dashboard
+
+
+const App = (props) => (
+    <Router>
         <div id="container">
-            <AppNavbar />
-            <Login />
+            <Navbar collapseOnSelect style={{ marginBottom: '0', borderRadius: '0' }}>
+                <AppLogoHeader logo={props.logo} />
+                <Navbar.Collapse>
+                    <Nav>
+                        <LinkContainer to="/dashboard"><NavItem eventKey={1}>Dashboard</NavItem></LinkContainer>
+                        <LinkContainer to="/"><NavItem eventKey={2}>Home (login)</NavItem></LinkContainer>
+                        <NavDropdown eventKey={3} title='Dropdown' id='basic-nav-dropdown'>
+                            <MenuItem eventKey={3.1}>Something1</MenuItem>
+                            <MenuItem eventKey={3.2}>Something2</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.3}>Something3</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Route exact path="/" component={Login} logo={props.logo} />
+            {routes.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)}
         </div>
-    );
-}*/
-
-const App = () => {
-    return (
-        <Switch>
-            <Route exact path="/" component={Login} />
-            <Route
-                path="/dashboard"
-                render={props => <Dashboard {...props} />}
-            />
-        </Switch>
-    );
-};
+    </Router>
+);
 
 export default App;
