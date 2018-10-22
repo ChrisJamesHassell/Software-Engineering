28a0efaa3f5cd740daf5802e459222845f2ceaeb
Chris <Chris@192.168.1.2>
2018-10-22 09:13:05 -0500

    Edit(User): Added date of birth field

---
+++ PlatypusApi/src/main/java/platypus/api/models/User.java
@@ -0,0 +1,89 @@
+package platypus.api.models;
+
+public class User {
+
+	private int userId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String username;
+	private String password;
+	
+	public User() {
+		
+	}
+	
+	public User(String fn, String ln, String e, String un, String p) {
+		this.firstName = fn;
+		this.lastName = ln;
+		this.email = e;
+		this.username = un;
+		this.password = p;
+	}
+	
+	public User(int u, String fn, String ln, String e, String un, String p) {
+		this.userId = u;
+		this.firstName = fn;
+		this.lastName = ln;
+		this.email = e;
+		this.username = un;
+		this.password = p;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", username=" + username + ", password=" + password + "]";
+	}
+
+	
+	
+}
