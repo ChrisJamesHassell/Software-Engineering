f34d12f175bb584610e32523f75eb8e3456d6025
Jonathan Penton <jonathan@penton.site>
2018-10-24 10:29:06 -0500

    Configure redux Provider

--- PlatypusWeb/src/index.js
+++ PlatypusWeb/src/index.js
@@ -1,18 +1,21 @@
 import * as serviceWorker from './serviceWorker';
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { routes } from './scripts/routes';
+import configureStore from './scripts/store/configureStore';
 import App from './scripts/components/Pages/App';
 import './css/global/bootstrap.css';
 import './css/style.css';
-import logo from './images/icons/logo_fill_white.svg'
+import logo from './images/icons/logo_fill_white.svg';
 
-
-//const store = configureStore();
-const baseURI = "https://www.platypus.null-terminator.com/api/";
+const store = configureStore();
+const baseURI = 'https://www.platypus.null-terminator.com/api/';
 
 render(
-    <App routes={routes} logo={logo} baseURI={baseURI} />,
+    <Provider store={store}>
+        <App routes={routes} logo={logo} baseURI={baseURI} />
+    </Provider>,
     document.getElementById('root')
 );
 
