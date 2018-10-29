2ed606e243d981574cb51051af54b2a0dcc9efc2
Dnfforever <43074070+Dnfforever@users.noreply.github.com>
2018-10-29 09:36:56 -0500

    Add files via upload

--- PlatypusDatabase/Relational/belongs_to.sql
+++ PlatypusDatabase/Relational/belongs_to.sql
@@ -11,18 +11,13 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-
--- Dumping database structure for platypus
-CREATE DATABASE IF NOT EXISTS `platypus` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `platypus`;
-
 -- Dumping structure for table platypus.belongs_to
 CREATE TABLE IF NOT EXISTS `belongs_to` (
   `groupID` int(11) unsigned NOT NULL,
   `userID` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`groupID`),
+  PRIMARY KEY (`groupID`,`userID`),
   KEY `FK_belongs_to_user` (`userID`),
-  CONSTRAINT `FK_belongs_to_group` FOREIGN KEY (`groupID`) REFERENCES `group` (`GroupID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_belongs_to_group` FOREIGN KEY (`groupID`) REFERENCES `groups` (`GroupID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_belongs_to_user` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
