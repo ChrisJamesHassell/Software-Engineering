c29cb78235d3f1904f96371199a0c4042cee9273
Dnfforever <43074070+Dnfforever@users.noreply.github.com>
2018-10-29 16:18:53 -0500

    Add files via upload

--- PlatypusDatabase/Relational/hasevent.sql
+++ PlatypusDatabase/Relational/hasevent.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS `hasevent` (
   `pinned` enum('0','1') NOT NULL DEFAULT '0',
   `notification` date DEFAULT NULL,
   PRIMARY KEY (`groupID`,`eventID`),
-  KEY `eventID` (`eventID`),
+  UNIQUE KEY `eventID` (`eventID`),
   CONSTRAINT `FK_hasevent_events` FOREIGN KEY (`eventID`) REFERENCES `events` (`eventID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_hasevent_groups` FOREIGN KEY (`groupID`) REFERENCES `groups` (`GroupID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
