17dee5c6643c7932fcec4252c429d0f478b070a7
Jon Sweeney <jon.sweeney501@gmail.com>
2018-11-02 14:24:22 -0700

    Update(AuthFilter)

--- PlatypusApi/src/main/java/platypus/api/handlers/AuthFilter.java
+++ PlatypusApi/src/main/java/platypus/api/handlers/AuthFilter.java
@@ -3,7 +3,6 @@ package platypus.api.handlers;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
-import platypus.api.models.UserTuple;
 import spark.Filter;
 import spark.Request;
 import spark.Response;
@@ -13,46 +12,34 @@ public class AuthFilter implements Filter {
 	public static final String TOKEN_COOKIE = "tokepi";
 	public static final String USERNAME = "USERNAME";
 
-	private final ConcurrentHashMap<String, UserTuple> sessions;
+	private final ConcurrentHashMap<String, String> sessions;
 
 	public AuthFilter() {
 		this.sessions = new ConcurrentHashMap<>(); // Has to be concurrent cause everything is sharing this filter ==>
 													// Thread safe
 	}
 
-	public String createSession(String userName, int id) {
+	public String createSession(String userName) {
 		String token = UUID.randomUUID().toString() + "." + UUID.randomUUID().toString();
-		UserTuple user = new UserTuple(userName, id);
-		sessions.put(token, user);
+		sessions.put(token, userName);
 		return token;
 	}
 	
-	/* 
-	 *  Returns the username + id corresponding to the request cookie.
-	 */ 
-	public UserTuple getUser(String cookie) {
-		UserTuple user = sessions.get(cookie);
-		return user;
-	}
+
 	
 	@Override
 	public void handle(Request request, Response response) throws Exception {
 		String token = request.cookie(TOKEN_COOKIE);
-	
+		System.out.println("Token cookie: " + token);
 		if(token == null) {
 			throw Spark.halt(401, "Cookie not found");
 		}
-		
-		UserTuple user = sessions.get(token);
-		String userName = user.getUsername();
-		
+		String userName = sessions.get(token);
 		if (userName == null) {
 			throw Spark.halt(401, "This can be JSON later, but means unauthorized");
 		}
 		request.attribute(USERNAME, userName);
-		
-		System.out.println(this.sessions.toString());
-		
+
 	}
 
 }
