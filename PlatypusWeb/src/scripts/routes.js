b79fb6f9abffafdaf995a3bb93748360aa7e9275
Jonathan Penton <jonathan@penton.site>
2018-11-02 16:32:48 -0500

    Merge changes from origin/login-frontend-micah

--- PlatypusWeb/src/scripts/routes.js
+++ PlatypusWeb/src/scripts/routes.js
@@ -1,50 +1,47 @@
 import React from 'react';
-import Tasks from './components/Pages/Tasks';
-
+import { Route } from 'react-router-dom';
+import { Dashboard, Bus, Cart } from './components/Pages/Dashboard';
+import Category from './components/Pages/Category';
+import Login from './components/Pages/Login';
+import LoginForm from './components/Forms/LoginForm';
+import SignupForm from './components/Forms/SignupForm';
+import logo from '../images/icons/logo_fill_white.svg';
+// then our route config
 const routes = [
     {
-        path: "/dashboard",
-        name: "Dashboard",
-        exact: true,
-        sidebar: () => <div></div>,
-        main: () => <b>Main Dash Stuff</b>
+        path: '/login',
+        component: Login,
+        routes: [
+            {
+                path: '/login/login',
+                component: LoginForm,
+                logo: logo,
+            },
+            {
+                path: '/login/signup',
+                component: SignupForm,
+                logo: logo,
+            },
+        ],
     },
     {
-        path: "/dashboard/tasks",
-        name: "Tasks",
-        sidebar: () => <div></div>,
-        main: () => <Tasks />
+        path: '/dashboard',
+        component: Dashboard,
+        routes: [
+            {
+                path: '/dashboard/bus',
+                component: Bus,
+            },
+            {
+                path: '/dashboard/cart',
+                component: Cart,
+            },
+            {
+                path: '/dashboard/:category',
+                component: Category,
+            },
+        ],
     },
-    {
-        path: "/dashboard/events",
-        name: "Events",
-        sidebar: () => <div></div>,
-        main: () => <b>Events Page</b>
-    },
-    {
-        path: "/dashboard/appliances",
-        name: "Appliances",
-        sidebar: () => <div></div>,
-        main: () => <b>Home and Appliances Page</b>
-    },
-    {
-        path: "/dashboard/medical",
-        name: "Medical",
-        sidebar: () => <div></div>,
-        main: () => <b>Medical Page</b>
-    },
-    {
-        path: "/dashboard/auto",
-        name: "Auto",
-        sidebar: () => <div></div>,
-        main: () => <b>Home and Appliances Page</b>
-    },
-    {
-        path: "/dashboard/meals",
-        name: "Meals",
-        sidebar: () => <div></div>,
-        main: () => <b>Meals</b>
-    }
 ];
 export default routes;
 // const routes = [
@@ -84,19 +81,16 @@ export default routes;
 //     }
 // ];
 
-// // wrap <Route> and use this everywhere instead, then when
-// // sub routes are added to any route it'll work
-// const RouteWithSubRoutes = (route) => (
-//     <Route
-//         path={route.path}
-//         render={(props) => (
-//             // pass the sub-routes down to keep nesting
-//             <route.component {...props} routes={route.routes} />
-//         )}
-//     />
-// );
+// wrap <Route> and use this everywhere instead, then when
+// sub routes are added to any route it'll work
+const RouteWithSubRoutes = route => (
+    <Route
+        path={route.path}
+        render={props => (
+            // pass the sub-routes down to keep nesting
+            <route.component {...props} routes={route.routes} />
+        )}
+    />
+);
 
-// export {
-//     routes,
-//     RouteWithSubRoutes
-// }
+export { routes, RouteWithSubRoutes };
