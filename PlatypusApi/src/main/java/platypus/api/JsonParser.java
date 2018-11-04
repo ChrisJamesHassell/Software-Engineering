da279d5d85274a8dfbe6dfdb9b7100cd6ed24dbf
Jonathan Penton <jonathan@penton.site>
2018-11-03 21:39:59 -0500

    Revert JsonParser.java to fd840e5199a6123cb528f9aa2ba42f8f5bafd8d2

--- PlatypusApi/src/main/java/platypus/api/JsonParser.java
+++ PlatypusApi/src/main/java/platypus/api/JsonParser.java
@@ -1,18 +1,12 @@
 package platypus.api;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 
 public class JsonParser {
 
 	private static final Gson gson = new Gson();
-	
-	public static <T> T getObject(Class<T> type, String json){
+
+	public static <T> T getObject(Class<T> type, String json) {
 		return gson.fromJson(json, type);
-	}
-	
-	// Append one JsonObject with another.
-	public static String appendJson(JsonObject j1, JsonObject j2) {
-		j1.geta
 	}
 }
