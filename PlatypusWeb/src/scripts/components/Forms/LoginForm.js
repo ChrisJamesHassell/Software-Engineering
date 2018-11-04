12d69b69084800272fac7046bb81a52403aa5236
Jonathan Penton <jonathan@penton.site>
2018-11-03 22:23:26 -0500

    Fixed API URLs

--- PlatypusWeb/src/scripts/components/Forms/LoginForm.js
+++ PlatypusWeb/src/scripts/components/Forms/LoginForm.js
@@ -1,67 +1,69 @@
 import React from 'react';
-import { FormGroup, Button, Glyphicon, InputGroup } from 'react-bootstrap';
+import {
+  FormGroup, Button, Glyphicon, InputGroup,
+} from 'react-bootstrap';
 import TextInput from './TextInput';
 import { path } from '../../fetchHelpers';
 
 export default class LoginForm extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-        this.state = {
-            route: path + '/user/login',
-            data: {
-                "username": "",
-                "pass": ""
-            },
-            response: {},
-            isSuccess: false
-        }
-        this.updateVals = this.updateVals.bind(this);
-    }
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      route: `${path}/user/login/`,
+      data: {
+        username: '',
+        pass: '',
+      },
+      response: {},
+      isSuccess: false,
+    };
+    this.updateVals = this.updateVals.bind(this);
+  }
 
-    updateVals(id, value) {
-        value.length < 1 && this.props.clearErrorAlert() // clear error alert if field is cleared
-        var temp = this.state.data;
-        temp[id] = value;
-        var data = Object.assign(this.state.data, temp);
-        this.setState({ data: data });
-    }
+  updateVals(id, value) {
+    value.length < 1 && this.props.clearErrorAlert(); // clear error alert if field is cleared
+    const temp = this.state.data;
+    temp[id] = value;
+    const data = Object.assign(this.state.data, temp);
+    this.setState({ data });
+  }
 
-    handleClick() {
-        this.props.login(this.state.route, this.state.data);
-    }
+  handleClick() {
+    this.props.login(this.state.route, this.state.data);
+  }
 
-    render() {
-        return (
-            <form action={this.state.route} method="POST" encType="multipart/form-data" onKeyUp={ev => ev.keyCode === 13 && this.handleClick()}>
-                <FormGroup controlId="username">
-                    <InputGroup>
-                        <InputGroup.Addon><Glyphicon glyph="user" /></InputGroup.Addon>
-                        <TextInput
-                            type={'text'}
-                            name={'username'}
-                            label={'User Name'}
-                            required={true}
-                            maxLength={"32"}
-                            placeholder={'Enter your user name'}
-                            updateVals={this.updateVals} />
-                    </InputGroup>
-                </FormGroup>
+  render() {
+    return (
+      <form action={this.state.route} method="POST" encType="multipart/form-data" onKeyUp={ev => ev.keyCode === 13 && this.handleClick()}>
+        <FormGroup controlId="username">
+          <InputGroup>
+            <InputGroup.Addon><Glyphicon glyph="user" /></InputGroup.Addon>
+            <TextInput
+              type={'text'}
+              name={'username'}
+              label={'User Name'}
+              required={true}
+              maxLength={'32'}
+              placeholder={'Enter your user name'}
+              updateVals={this.updateVals} />
+          </InputGroup>
+        </FormGroup>
 
-                <FormGroup controlId="pass" >
-                    <InputGroup>
-                        <InputGroup.Addon><Glyphicon glyph="lock" /></InputGroup.Addon>
-                        <TextInput
-                            type={'password'}
-                            name={'password'}
-                            label={'Password'}
-                            required={true}
-                            maxLength={"32"}
-                            placeholder={'Enter your password'}
-                            updateVals={this.updateVals} />
-                    </InputGroup>
-                </FormGroup>
-                <Button type="button" bsStyle='success' style={{ width: '100%' }} onClick={this.handleClick.bind(this)} disabled={this.state.data.username.length < 1 || this.state.data.pass.length < 1}>Login</Button>
-            </form>
-        );
-    }
+        <FormGroup controlId="pass" >
+          <InputGroup>
+            <InputGroup.Addon><Glyphicon glyph="lock" /></InputGroup.Addon>
+            <TextInput
+              type={'password'}
+              name={'password'}
+              label={'Password'}
+              required={true}
+              maxLength={'32'}
+              placeholder={'Enter your password'}
+              updateVals={this.updateVals} />
+          </InputGroup>
+        </FormGroup>
+        <Button type="button" bsStyle='success' style={{ width: '100%' }} onClick={this.handleClick.bind(this)} disabled={this.state.data.username.length < 1 || this.state.data.pass.length < 1}>Login</Button>
+      </form>
+    );
+  }
 }
--- PlatypusWeb/src/scripts/components/Forms/SignupForm.js
+++ PlatypusWeb/src/scripts/components/Forms/SignupForm.js
@@ -1,120 +1,118 @@
 import React from 'react';
-import { FormGroup, Button, FormControl, HelpBlock, Alert } from 'react-bootstrap';
+import {
+  FormGroup, Button, FormControl, HelpBlock, Alert,
+} from 'react-bootstrap';
 import TextInput from './TextInput';
-import { path } from '../../fetchHelpers'
+import { path } from '../../fetchHelpers';
 
 // This is required to match correctly
 const regex = {
-    'username': /^(?=.*[a-zA-Z])[A-Za-z\d]{8,32}$/g,
-    'pass': /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,32}$/g,
-    'firstName': /^(?=.*[A-Za-z])[A-Za-z]{1,32}$/g,
-    'lastName': /^(?=.*[A-Za-z])[A-Za-z]{1,32}$/g,
-    'email': /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/g,
-    'dateOfBirth': /^.+$/g
-}
+  username: /^(?=.*[a-zA-Z])[A-Za-z\d]{8,32}$/g,
+  pass: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,32}$/g,
+  firstName: /^(?=.*[A-Za-z])[A-Za-z]{1,32}$/g,
+  lastName: /^(?=.*[A-Za-z])[A-Za-z]{1,32}$/g,
+  email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/g,
+  dateOfBirth: /^.+$/g,
+};
 
 export default class SignupForm extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-        this.state = {
-            id: null,
-            isDisabled: true,
-            route: path + '/user/create',
-            data: {
-                "username": {
-                    validation: null,
-                    value: null,
-                    requirements: 'Must be between 8-32 chars in length with no special chars',
-                    helpText: null
-                },
-                "pass": {
-                    validation: null,
-                    value: null,
-                    requirements: 'Must be between 8-32 chars in length with 1 uppercase, 1 lowercase, 1 number, and no special chars',
-                    helpText: null
-                },
-                "firstName": {
-                    validation: null,
-                    value: null,
-                    requirements: 'Must be between 1-32 chars in length and only contain letters',
-                    helptext: null
-                },
-                "lastName": {
-                    validation: null,
-                    value: null,
-                    requirements: 'Must be between 1-32 chars in length and only contain letters',
-                    helptext: null
-                },
-                "email": {
-                    validation: null,
-                    value: null,
-                    requirements: 'That is not a valid email address',
-                    helptext: null
-                },
-                "dateOfBirth": {
-                    validation: null,
-                    value: null,
-                    requirements: '',
-                    helptext: null
-                }
-            },
-            error: ""
-        }
-        this.updateVals = this.updateVals.bind(this);
-        this.setValidationState = this.setValidationState.bind(this);
-        this.handleClick = this.handleClick.bind(this);
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      id: null,
+      isDisabled: true,
+      route: `${path}/user/create/`,
+      data: {
+        username: {
+          validation: null,
+          value: null,
+          requirements: 'Must be between 8-32 chars in length with no special chars',
+          helpText: null,
+        },
+        pass: {
+          validation: null,
+          value: null,
+          requirements: 'Must be between 8-32 chars in length with 1 uppercase, 1 lowercase, 1 number, and no special chars',
+          helpText: null,
+        },
+        firstName: {
+          validation: null,
+          value: null,
+          requirements: 'Must be between 1-32 chars in length and only contain letters',
+          helptext: null,
+        },
+        lastName: {
+          validation: null,
+          value: null,
+          requirements: 'Must be between 1-32 chars in length and only contain letters',
+          helptext: null,
+        },
+        email: {
+          validation: null,
+          value: null,
+          requirements: 'That is not a valid email address',
+          helptext: null,
+        },
+        dateOfBirth: {
+          validation: null,
+          value: null,
+          requirements: '',
+          helptext: null,
+        },
+      },
+      error: '',
+    };
+    this.updateVals = this.updateVals.bind(this);
+    this.setValidationState = this.setValidationState.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidUpdate(prevProps, nextProps) {
+    const stateChanged = JSON.stringify(nextProps) !== JSON.stringify(this.state);
+    const objCount = Object.keys(this.state.data).length;
+    if (stateChanged) {
+      let formValid = 0;
+      Object.keys(this.state.data).forEach((item) => {
+        formValid += (this.state.data[item].validation === 'success' ? 1 : 0);
+      });
+      this.setState({ isDisabled: formValid < objCount });
     }
+  }
 
-    componentDidUpdate(prevProps, nextProps) {
-        var stateChanged = JSON.stringify(nextProps) !== JSON.stringify(this.state);
-        var objCount = Object.keys(this.state.data).length;
-        if (stateChanged) {
-            var formValid = 0;
-            Object.keys(this.state.data).forEach((item) => {
-                formValid += (this.state.data[item].validation === 'success' ? 1 : 0)
-            })
-            this.setState({ isDisabled: formValid < objCount });
-        }
+  setValidationState(id, validProps) {
+    const mergedProps = Object.assign(this.state.data[id], validProps);
+    this.setState({ id }); // set the current input item id we are on
+    this.setState(state => state.data[id] = mergedProps);
+  }
+
+  updateVals(id, value) {
+    const match = value.match(regex[id]); // Get whether or not the input is valid
+
+    if (match) // If there WAS a match, set appropriate values
+    { this.setValidationState(id, { validation: 'success', value, helpText: null }); } else { // If there was NOT a match
+      let [validation, helpText] = [null, null]; // Clear the error if the field is empty
+      value.length < 1 && this.props.clearErrorAlert(); // clear whole form error alert if field is cleared
+      if (value.length > 0) [validation, helpText] = ['error', this.state.data[id].requirements];
+      this.setValidationState(id, { validation, value: null, helpText });
     }
+  }
 
-    setValidationState(id, validProps) {
-        var mergedProps = Object.assign(this.state.data[id], validProps);
-        this.setState({ id: id });  // set the current input item id we are on
-        this.setState((state) => {  // merge the updated properties
-            return state.data[id] = mergedProps;
-        });
-    }
+  handleClick() {
+    const data = {};
+    Object.keys(this.state.data).forEach((key) => {
+      data[key] = this.state.data[key].value;
+    });
+    this.props.login(this.state.route, data);
+  }
 
-    updateVals(id, value) {
-        var match = value.match(regex[id]);     // Get whether or not the input is valid
-        
-        if (match)      // If there WAS a match, set appropriate values
-            this.setValidationState(id, { validation: 'success', value: value, helpText: null });
-
-        else {      // If there was NOT a match
-            let [validation, helpText] = [null, null];  // Clear the error if the field is empty
-            value.length < 1 && this.props.clearErrorAlert() // clear whole form error alert if field is cleared
-            if (value.length > 0) [validation, helpText] = ['error', this.state.data[id].requirements];
-            this.setValidationState(id, { validation: validation, value: null, helpText: helpText });
-        }
-    }
-
-    handleClick() {
-        var data = {};
-        Object.keys(this.state.data).forEach(key => {
-            data[key] = this.state.data[key].value
-        })
-        this.props.login(this.state.route, data);
-    }
-
-    render() {
-        var userNameHelp = this.state.data['username'].helpText;
-        var passwordHelp = this.state.data['pass'].helpText;
-        return (
+  render() {
+    const userNameHelp = this.state.data.username.helpText;
+    const passwordHelp = this.state.data.pass.helpText;
+    return (
             <form>
                 <FormGroup
                     controlId="username"
-                    validationState={this.state.data['username'].validation}
+                    validationState={this.state.data.username.validation}
                 >
                     <TextInput
                         type={'text'}
@@ -128,7 +126,7 @@ export default class SignupForm extends React.Component {
 
                 <FormGroup
                     controlId="pass"
-                    validationState={this.state.data['pass'].validation}
+                    validationState={this.state.data.pass.validation}
                 >
                     <TextInput
                         type={'password'}
@@ -142,7 +140,7 @@ export default class SignupForm extends React.Component {
 
                 <FormGroup
                     controlId="firstName"
-                    validationState={this.state.data['firstName'].validation}
+                    validationState={this.state.data.firstName.validation}
                 >
                     <TextInput
                         type={'text'}
@@ -154,7 +152,7 @@ export default class SignupForm extends React.Component {
 
                 <FormGroup
                     controlId="lastName"
-                    validationState={this.state.data['lastName'].validation}
+                    validationState={this.state.data.lastName.validation}
                 >
                     <TextInput
                         type={'text'}
@@ -166,7 +164,7 @@ export default class SignupForm extends React.Component {
 
                 <FormGroup
                     controlId="dateOfBirth"
-                    validationState={this.state.data['dateOfBirth'].validation}
+                    validationState={this.state.data.dateOfBirth.validation}
                 >
                     <TextInput
                         type={'date'}
@@ -178,7 +176,7 @@ export default class SignupForm extends React.Component {
 
                 <FormGroup
                     controlId="email"
-                    validationState={this.state.data['email'].validation}
+                    validationState={this.state.data.email.validation}
                 >
                     <TextInput
                         type={'email'}
@@ -190,6 +188,6 @@ export default class SignupForm extends React.Component {
 
                 <Button bsStyle='success' style={{ width: '100%' }} onClick={this.handleClick} disabled={this.state.isDisabled}>Sign Up</Button>
             </form>
-        );
-    }
+    );
+  }
 }
