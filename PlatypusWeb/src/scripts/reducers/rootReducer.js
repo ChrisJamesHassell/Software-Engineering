1441c18d20f30553e7460bde1e7eef07dc0efb76
Jonathan Penton <jonathan@penton.site>
2018-11-03 21:24:40 -0500

    Added documents, events to redux store

--- PlatypusWeb/src/scripts/reducers/rootReducer.js
+++ PlatypusWeb/src/scripts/reducers/rootReducer.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 
+import documentsReducer from './documentsReducer';
+import eventsReducer from './eventsReducer';
 import tasksReducer from './tasksReducer';
 
 export default combineReducers({
+    documents: documentsReducer,
+    events: eventsReducer,
     tasks: tasksReducer,
 });
