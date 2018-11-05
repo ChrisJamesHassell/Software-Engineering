01064901a4f6559e7e2af0ba2b515cb320d1bc36
Jon Sweeney <jon.sweeney501@gmail.com>
2018-11-05 12:02:53 -0800

    Add(DocumentApi) : Added in basic functionality for documents
    Still need to implement the storage of files on the file system.

---
+++ PlatypusApi/src/main/java/platypus/api/handlers/DocumentApi.java
@@ -0,0 +1,152 @@
+package platypus.api.handlers;
+import spark.Request;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.zaxxer.hikari.HikariDataSource;
+public class DocumentApi {
+	
+	
+	// TODO: -Set up the response body to return CacheEntry + document stuff
+	//		 -Test more extensively if needed
+	public static JsonResponse addDoc(HikariDataSource ds, Request req) throws SQLException  {
+		Connection conn = null;
+		CallableStatement stmt = null;
+		
+		try {
+			// Parse request body to get the document stuff.
+			Gson gson = new Gson();
+			JsonObject jsonO = gson.fromJson(req.body(), JsonObject.class);
+			
+			JsonObject user = jsonO.get("user").getAsJsonObject();
+			JsonObject group = jsonO.get("group").getAsJsonObject();
+			JsonObject document = jsonO.get("document").getAsJsonObject();
+			
+			conn = ds.getConnection();
+
+			
+			//Prepare the call from request body
+			stmt = conn.prepareCall("{call insertDoc(?, ?, ?, ?, ?, ?, ?, ?)}");
+			stmt.setString(1, document.get("pinned").getAsString());
+			stmt.setString(2, document.get("notification").getAsString());
+			stmt.setInt(3, group.get("groupID").getAsInt());
+			stmt.setString(4, document.get("name").getAsString());
+			stmt.setString(5, document.get("description").getAsString());
+			stmt.setString(6, document.get("category").getAsString());
+			stmt.setString(7, document.get("fileName").getAsString());
+			stmt.setString(8, document.get("expirationDate").getAsString());
+			
+			stmt.executeUpdate();
+			
+			// TODO: Actually insert the document on the file system. :(
+			
+			// Need to return CacheEntry for this user + the document stuff
+			return new JsonResponse("SUCCESS", "", "Successfully inserted document.");
+		} catch (SQLException sqlE) {
+			return new JsonResponse("ERROR", "", "SQLError in Add document");
+		}
+		finally {
+			conn.close();
+		} 
+	}
+	
+	public static JsonResponse editDoc(HikariDataSource ds, Request req) throws SQLException {
+
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
+		try {
+			// Parse request body to get the document stuff.
+			Gson gson = new Gson();
+			JsonObject jsonO = gson.fromJson(req.body(), JsonObject.class);
+			JsonObject document = jsonO.get("document").getAsJsonObject();
+			
+			
+			
+			conn = ds.getConnection();
+			
+			//Prepare the call from request body
+			stmt = conn.prepareStatement("UPDATE documents SET name = ?, description = ?, expirationDate = ? WHERE docID = ?");
+			stmt.setString(1, document.get("name").getAsString());
+			stmt.setString(2, document.get("description").getAsString());
+			stmt.setString(3, document.get("expirationDate").getAsString());
+			stmt.setInt(4, document.get("documentID").getAsInt());
+			
+			
+			int ret = stmt.executeUpdate();		
+			System.out.println(ret);
+			// Successful update
+			if (ret == 1) {
+				// Should not need to touch anything on the file system here, since we only touched the database entry for it.
+				// TODO: Build the CacheEntry + new document stuff.
+				return new JsonResponse("SUCCESS", "", "Successfully edited document");
+			} else {
+				// The documentID does not exist.
+				return new JsonResponse("FAIL", "", "The document does not exist");
+			}
+			
+		} catch (SQLException sqlE) {
+			return new JsonResponse("ERROR", "", "SQLError in Editdocument");
+		}
+		finally {
+			conn.close();
+		} 
+	}
+	
+	
+	
+	// Successfully removes the document from all appropriate tables.
+	// TODO: -Build the response correctly.
+	//		 -Test more extensively.
+	public static JsonResponse removeDoc(HikariDataSource ds, Request req) throws SQLException {
+		Connection conn = null;
+		CallableStatement stmt = null;
+	
+		
+		
+		try {
+			// Parse request body to get the document stuff.
+			Gson gson = new Gson();
+			JsonObject jsonO = gson.fromJson(req.body(), JsonObject.class);
+			
+			// Still necessary to build the CacheEntry response.
+			JsonObject user = jsonO.get("user").getAsJsonObject();
+			JsonObject group = jsonO.get("group").getAsJsonObject();
+			JsonObject document = jsonO.get("document").getAsJsonObject();
+			
+			conn = ds.getConnection();
+
+			
+			//Prepare the call from request body
+			stmt = conn.prepareCall("{call delDoc(?)}");
+			stmt.setInt(1, document.get("documentID").getAsInt());
+			int ret = stmt.executeUpdate();
+			
+			
+			
+			if (ret != 0) {
+				// TODO: Need to return CacheEntry for this user + the documentInfo
+				// -Delete the file off the file system as well.
+					return new JsonResponse("SUCCESS", "", "Successfully deleted document.");	
+			} else {
+				// There is no document with that documentID
+					return new JsonResponse("FAIL", "", "There is no document with that ID, failed document deletion.");
+			}
+			
+			
+		} catch (SQLException sqlE) {
+			return new JsonResponse("ERROR", "", "SQLError in Add document");
+		}
+		finally {
+			conn.close();
+		} 
+	}
+
+	
+	
+}
+
