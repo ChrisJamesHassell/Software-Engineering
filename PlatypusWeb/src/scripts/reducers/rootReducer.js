c1447300ba24b99d4cefa1e2a961e3c98f49f926
Jonathan Penton <jonathan@penton.site>
2018-11-07 12:04:52 -0600

    Add user reducer

--- PlatypusWeb/src/scripts/reducers/rootReducer.js
+++ PlatypusWeb/src/scripts/reducers/rootReducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import documentsReducer from './documentsReducer';
 import eventsReducer from './eventsReducer';
 import tasksReducer from './tasksReducer';
+import userReducer from './userReducer';
 
 export default combineReducers({
   documents: documentsReducer,
   events: eventsReducer,
   tasks: tasksReducer,
+  user: userReducer,
 });
