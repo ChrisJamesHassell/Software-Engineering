769d22c224783a525495ae6452369964bea572f9
Jonathan Penton <jonathan@penton.site>
2018-11-08 20:56:43 -0600

    Remove storing user data in reducer
    It's already being stored in localStorage

--- PlatypusWeb/src/scripts/components/Pages/Login.js
+++ PlatypusWeb/src/scripts/components/Pages/Login.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { Button, Grid, Row, Col, Alert, Modal } from 'react-bootstrap';
+import {
+  Button, Grid, Row, Col, Alert, Modal,
+} from 'react-bootstrap';
+import { connect } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Route, Redirect } from 'react-router-dom';
 import LoginForm from '../Forms/LoginForm';
 import SignupForm from '../Forms/SignupForm';
 import logo from '../../../images/icons/icon_circle_white.svg';
 
-const LoginMobileContent = (props) => (
-  <Col id='login-logo' smHidden mdHidden lgHidden xs={12}>
-    <div><img src={props.logoUrl} id='logo-hidden' alt='white logo' /></div>
-    <div id='login-logo-brand'>
-      <span id='brand-platy'>platy</span><span id='brand-pus'>pus</span>
+const LoginMobileContent = props => (
+  <Col id="login-logo" smHidden mdHidden lgHidden xs={12}>
+    <div>
+      <img src={props.logoUrl} id="logo-hidden" alt="white logo" />
+    </div>
+    <div id="login-logo-brand">
+      <span id="brand-platy">platy</span>
+      <span id="brand-pus">pus</span>
     </div>
   </Col>
 );
 
-const LoadingModal = (props) => (
+const LoadingModal = props => (
   <Modal show={props.loading}>
     <Modal.Body>
       <b>Loading...</b>
@@ -24,55 +30,56 @@ const LoadingModal = (props) => (
 );
 
 const LoginLargeContent = () => (
-  <Col id='login-extra' xsHidden md={8}>
+  <Col id="login-extra" xsHidden md={8}>
     <div>
       <h1>Organize.</h1>
       <h1>Plan.</h1>
       <h1>Live.</h1>
       <p>
-        Hey, adulting is hard. We get it. That's why Platypus provides
-        a sleek, modern interface to help you adult at maximum efficiency.
-            </p>
-      <p><Button bsStyle='success' bsSize='large'>Learn More</Button></p>
+        Hey, adulting is hard. We get it. That's why Platypus provides a sleek, modern interface to
+        help you adult at maximum efficiency.
+      </p>
+      <p>
+        <Button bsStyle="success" bsSize="large">
+          Learn More
+        </Button>
+      </p>
     </div>
   </Col>
 );
 
-const RowSpacer = () => (
-  <Row id='row-space'>
+const RowSpacer = () => <Row id="row-space" />;
 
-  </Row>
-);
-
-export default class Login extends React.Component {
+class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       redirect: false,
       error: '',
-      loading: false
-    }
+      loading: false,
+    };
     this.login = this.login.bind(this);
   }
 
   login(route, data) {
     this.setState({ loading: true });
-    var opts = {
+    const opts = {
       method: 'POST',
       credentials: 'include',
       headers: {
-        'Content-type': 'application/json'
+        'Content-type': 'application/json',
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     };
     fetch(route, opts)
-      .then(response => this.validateResponse(response)) // If not valid, skips rest and goes to catch
-      .then(validResponse => { return validResponse.json() })
+      // If not valid, skips rest and goes to catch
+      .then(response => this.validateResponse(response))
+      .then(validResponse => validResponse.json())
       .then(jsonResponse => this.handleJsonResponse(jsonResponse))
       .catch(error => this.logError(error));
   }
 
-  validateResponse(result) {
+  validateResponse = (result) => {
     if (!result.ok) throw Error(result.statusText);
     return result;
   };
@@ -80,9 +87,7 @@ export default class Login extends React.Component {
   handleJsonResponse(response) {
     this.setState({ loading: false });
     const {
-      data: {
-        documents, events, tasks, ...user
-      },
+      data: { documents, events, tasks },
       status,
     } = response;
 
@@ -98,25 +103,21 @@ export default class Login extends React.Component {
       type: 'ADD_TASKS',
       payload: tasks,
     });
-    this.props.dispatch({
-      type: 'UPDATE_USER',
-      payload: user,
-    });
 
     const isSuccess = status === 'SUCCESS';
     const data = Object.assign({}, response.data);
-    Object.keys(data).forEach((key) =>{
-      let value = JSON.stringify(data[key]);
+    Object.keys(data).forEach((key) => {
+      const value = JSON.stringify(data[key]);
       localStorage.setItem(key, value);
-    })
-    
+    });
+
     if (isSuccess) this.setState({ redirect: true });
     else this.logError(response.message);
   }
 
   logError(error) {
     this.setState({ loading: false });
-    var err = error.toString();
+    let err = error.toString();
     if (err.includes('Failed to fetch')) err = 'There was a problem connecting to the server. Please contact the service administrator.';
     this.setState({ error: err });
   }
@@ -128,7 +129,7 @@ export default class Login extends React.Component {
   render() {
     const isLogin = this.props.location.pathname === '/login';
     const { redirect } = this.state;
-    
+
     if (redirect) {
       window.location.reload();
       return <Redirect to="/dashboard" />;
@@ -181,8 +182,7 @@ export default class Login extends React.Component {
                     </Button>
                   </LinkContainer>
                 </div>
-                <div>
-                </div>
+                <div />
               </div>
             </Col>
           </Row>
@@ -191,3 +191,5 @@ export default class Login extends React.Component {
     );
   }
 }
+
+export default connect()(Login);
